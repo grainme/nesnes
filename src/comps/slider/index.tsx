0e066667b30bb7d7d92fa7c696d9e43472cda715
grainme <marouanebouf3@gmail.com>
2024-04-06 14:12:20 +0000

    update

--- src/comps/slider/index.tsx
+++ src/comps/slider/index.tsx
@@ -2,10 +2,9 @@
 import styles from "./style.module.css";
 import { useState } from "react";
 
-export default function index(project: any) {
+export default function index({ project }: any) {
   const [, setIsActive] = useState(false);
 
-  const { title1 } = project;
   return (
     <div
       onMouseEnter={() => {
@@ -17,16 +16,8 @@ export default function index(project: any) {
       className={styles.project}
     >
       <p className="font-cooper text-[70px] text-[#c4c2c2] hover:text-black">
-        {title1}
+        {project.title1}
       </p>
-      {/* <motion.div
-        variants={anim}
-        animate={isActive ? "open" : "closed"}
-        className={styles.imgContainer}
-      >
-        <img src={`/medias/${src}`}></img>
-      </motion.div> */}
-      {/* <p className="font-ibm text-[80px]">{title2}</p> */}
     </div>
   );
 }
